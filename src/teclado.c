79946658c8debca454d10a2bc2a5651edb7fb0e6
loslocos817yt-star <loslocos817yt@gmail.com>
2026-07-13 03:17:03 -0600

    Update: Implementado BE (Basic Evil) y soporte de símbolos en teclado

--- src/teclado.c
+++ src/teclado.c
@@ -6,10 +6,15 @@ extern unsigned char inb(unsigned short port);
 extern void redraw_line(const char* cmd, int len, int edit_pos);
 extern void k_strcpy(char* dest, const char* src);
 
+// ==========================================
+// TECLADO MODIFICADO PARA BASIC EVIL (BE)
+// 9 -> (  |  0 -> )  |  - -> * |  ' -> "
+// [ -> {  |  ] -> }
+// ==========================================
 const char scancode_map[128] = {
-    0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
-    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
-    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\',
+    0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '(', ')', '*', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '\n',
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', '`', 0, '\\',
     'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
 };
 
